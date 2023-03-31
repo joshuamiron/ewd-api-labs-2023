3138c652b7383fb9ab0dbafabceafd9192007d8b
Joshua W Miron <jmiron@gmail.com>
2023-03-31 00:42:07 +0000

    Lab exericse 3 - improved validation and disallowed duplicate favourite entries.

--- src/accounts/validators/index.js
+++ src/accounts/validators/index.js
@@ -2,10 +2,12 @@
 import Joi from 'joi';
 
 const accountSchema = Joi.object({
+    firstName: Joi.string().min(1).max(30).required(),
+    lastName: Joi.string().min(1).max(30).required(),
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(4).required(),
-    firstName: Joi.string().min(1).required(),
-    lastName: Joi.string().min(1).required()
+    password: Joi.string().min(7).max(20)
+        .pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+=]).+$/)
+        .required()
 });
 
 
