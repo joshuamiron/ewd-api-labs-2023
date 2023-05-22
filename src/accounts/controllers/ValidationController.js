55a209d64ddbbf2140ddd40a54949e5d4cd2b2cf
Joshua W Miron <jmiron@gmail.com>
2023-05-22 16:03:09 +0000

    Assignment 2 - trying to get Swagger workign. Updated version # in swagger.json

--- src/accounts/controllers/ValidationController.js
+++ src/accounts/controllers/ValidationController.js
@@ -1,20 +1,18 @@
 export default (dependencies) => {
+  const { validators } = dependencies;
 
-    const { validators } = dependencies;
+  const validateAccount = async (request, response, next) => {
+    // Input
+    try {
+      const validated = await validators["account"].validateAsync(request.body);
+      request.body = validated;
+      next();
+    } catch (err) {
+      next(new Error(`Invalid Data ${err.message}`));
+    }
+  };
 
-    const validateAccount = async (request, response, next) => {
-        // Input
-        try {
-            const validated = await validators['account'].validateAsync(request.body);
-            request.body = validated;
-            next();
-        } catch (err) {
-
-            next(new Error(`Invalid Data ${err.message}`));
-        }
-    };
-
-    return {
-        validateAccount
-    };
+  return {
+    validateAccount,
+  };
 };
--- src/accounts/validators/index.js
+++ src/accounts/validators/index.js
@@ -1,14 +1,15 @@
 //* validators/register.validator.js
-import Joi from 'joi';
+import Joi from "joi";
 
 const accountSchema = Joi.object({
-    firstName: Joi.string().min(1).max(30).required(),
-    lastName: Joi.string().min(1).max(30).required(),
-    email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(7).max(20)
-        .pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+=]).+$/)
-        .required()
+  firstName: Joi.string().min(1).max(30).required(),
+  lastName: Joi.string().min(1).max(30).required(),
+  email: Joi.string().email().lowercase().required(),
+  password: Joi.string()
+    .min(7)
+    .max(20)
+    .pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+=]).+$/)
+    .required(),
 });
 
-
-export default {account: accountSchema};
+export default { account: accountSchema };
