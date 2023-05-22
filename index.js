c2b6747e66613c697877a1bb93584931897d442c
Joshua W Miron <jmiron@gmail.com>
2023-05-22 16:46:37 +0000

    Assignment 2 - trying to get Swagger working.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import createPeopleRouter from './src/people/routes';
 import buildDependencies from "./src/config/dependencies";
 import db from './src/config/db';
 import errorHandler from './src/utils/ErrorHandler';
+import { serveSwaggerDocs } from './swagger.js';
+
 
 dotenv.config();
 
@@ -15,7 +17,6 @@ db.init();
 const app = express();
 
 const port = process.env.PORT;
-
 const dependencies = buildDependencies();
 
 app.use(express.json());
@@ -24,6 +25,8 @@ app.use('/api/accounts', createAccountsRouter(dependencies));
 app.use('/api/movies', createMoviesRouter(dependencies));
 app.use('/api/tv', createTVShowsRouter(dependencies));
 app.use('/api/people', createPeopleRouter(dependencies));
+
+serveSwaggerDocs(app);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
